82a2ab8c39779282c83f18d5792eb724e76b8734
nyxia91 <danielchristian0711@Gmail.com>
2019-02-25 04:48:51 +0700

    Some features with its UI

--- app/src/main/java/com/daraa/androidwarner/LocationActivity.java
+++ app/src/main/java/com/daraa/androidwarner/LocationActivity.java
@@ -1,9 +1,16 @@
 package com.daraa.androidwarner;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LocationActivity extends AppCompatActivity {
+
+    private TextView mSpeed;
+
+    private Location mLastLocation;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
